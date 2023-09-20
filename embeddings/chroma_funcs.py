099bbba5d234ac36ae66ef46c81a9c5ff1e2112b
samlhuillier <sam.jansa123@gmail.com>
2023-09-20 14:38:27 -0700

    inital work tieing up chroma

--- embeddings/chroma_funcs.py
+++ embeddings/chroma_funcs.py
@@ -3,6 +3,7 @@ from chromadb.utils import embedding_functions
 
 from datasets import load_dataset
 
+client = chromadb.PersistentClient(path="./chroma_store")
 
 # collection = client.get_or_create_collection(name="dataset")
 # print(collection.count())
@@ -33,9 +34,9 @@ def setup_collection(hf_dataset_name, feature_to_embed, dataset_split, collectio
 
 # TODO: this dataset_path could be both a huggingface name or a local path. Then we leverage transformers to pull it agnostically
 def get_closest_entries(
-    hf_dataset_name, feature_to_embed, query, client, n_results=5, dataset_split="train"
+    hf_dataset_name, feature_to_embed, query, n_results=5, dataset_split="train"
 ):
-    collection_name = hf_dataset_name.replace("/", "-")
+    collection_name = hf_dataset_name.replace("/", "-") + "-" + dataset_split
     # client.delete_collection(name=collection_name)
     collection = client.get_or_create_collection(name=collection_name)
     print("collection.count()", collection.count())
@@ -56,12 +57,10 @@ def get_closest_entries(
     return results
 
 
-client = chromadb.PersistentClient(path="./chroma")
-
-get_closest_entries(
-    "samlhuillier/sql-create-context-spider-intersect",
-    "question",
-    "get the best football players",
-    client,
-    n_results=3,
-)
+# get_closest_entries(
+#     "samlhuillier/sql-create-context-spider-intersect",
+#     "question",
+#     "get the best football players",
+#     n_results=3,
+#     dataset_split="train",
+# )
