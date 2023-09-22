5d1c07cf990d8240aae3df30f8314d7152cf708e
samlhuillier <sam.jansa123@gmail.com>
2023-09-22 14:17:36 -0700

    finally setup rag dataset augmenter working end2end

--- embeddings/chroma_funcs.py
+++ embeddings/chroma_funcs.py
@@ -16,20 +16,46 @@ client = chromadb.PersistentClient(path="./chroma_store")
 #     return collection
 
 
+def dataset_chunks(dataset, n):
+    """Yield successive n-sized chunks from the dataset."""
+    for i in range(0, len(dataset), n):
+        start_idx = i
+        end_idx = min(i + n, len(dataset))
+        yield dataset.select(range(start_idx, end_idx))
+
+
 def generate_knowledge_base_from_hf_dataset(
-    hf_dataset_name, embed_feature, dataset_split="train"
+    hf_dataset_name, embed_feature, emb_fn, dataset_split="train"
 ):
-    collection_name = hf_dataset_name.replace("/", "-") + "-" + dataset_split
-    collection = client.get_or_create_collection(name=collection_name)
+    # so here, we should set the name of the collection based on the emb_fn
+    print("embd_fn", emb_fn)
+    model_name = "default-emb-fn"
+    if hasattr(emb_fn, "_model_name"):
+        model_name = emb_fn._model_name
+    # emb_fn_name = emb_fn._model_name
+    collection_name = (
+        hf_dataset_name.replace("/", "-") + "-" + dataset_split + "-" + model_name
+    )[:62]
+
+    # client.delete_collection(collection_name)
+    collection = client.get_or_create_collection(
+        name=collection_name, embedding_function=emb_fn
+    )
 
     if collection.count() == 0:
         dataset = load_dataset(hf_dataset_name, split=dataset_split)
-        collection.add(
-            documents=dataset[embed_feature],
-            metadatas=[item for item in dataset],
-            ids=[f"id{i+1}" for i in range(len(dataset))],
-        )
-
+        chunk_size = 300
+        for chunk in dataset_chunks(dataset, chunk_size):
+            collection.add(
+                documents=chunk[embed_feature],
+                metadatas=[item for item in chunk],
+                ids=[f"id{i+1}" for i in range(len(chunk))],
+            )
+        # collection.add(
+        #     documents=dataset[embed_feature],
+        #     metadatas=[item for item in dataset],
+        #     ids=[f"id{i+1}" for i in range(len(dataset))],
+        # )
     return collection
 
 
