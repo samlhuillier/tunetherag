106ac1aefd5e327c8979cc066d317bb118509b86
samlhuillier <sam.jansa123@gmail.com>
2023-09-29 08:22:47 -0600

    clean up funcs

--- embeddings/chroma_funcs.py
+++ embeddings/chroma_funcs.py
@@ -6,16 +6,6 @@ from datasets import load_dataset
 client = chromadb.PersistentClient(path="./chroma_store")
 
 
-# def setup_collection(hf_dataset_name, feature_to_embed, dataset_split, collection):
-#     dataset = load_dataset(hf_dataset_name, split=dataset_split)
-#     collection.add(
-#         documents=dataset[feature_to_embed],
-#         metadatas=[item for item in dataset],
-#         ids=[f"id{i+1}" for i in range(len(dataset))],
-#     )
-#     return collection
-
-
 def dataset_chunks(dataset, n):
     """Yield successive n-sized chunks from the dataset."""
     for i in range(0, len(dataset), n):
@@ -70,30 +60,14 @@ def get_closest_entries(collection, query, embed_feature, n_results=5):
     return results
 
 
-# def old_get_closest_entries(
-#     hf_dataset_name, embed_feature, query, n_results=5, dataset_split="train"
-# ):
-#     collection_name = hf_dataset_name.replace("/", "-") + "-" + dataset_split
-#     collection = client.get_or_create_collection(name=collection_name)
-
-#     if collection.count() == 0:
-#         collection = setup_collection(
-#             hf_dataset_name, embed_feature, dataset_split, collection
-#         )
-
-#     results = collection.query(
-#         query_texts=[query],  # TODO: look into what multiple queries will do here.
-#         n_results=n_results,
-#         where={embed_feature: {"$ne": query}},  # exact match is train/val contamination
-#     )
-#     # TODO: throw error here if there are any exact matches
-#     return results
-
-
-# get_closest_entries(
-#     "samlhuillier/sql-create-context-spider-intersect",
-#     "question",
-#     "get the best football players",
-#     n_results=3,
-#     dataset_split="train",
-# )
+def get_random_entries(collection, embed_feature, n_results=1):
+    # so we just query the length of the collection then work
+    # total_items = collection.count()
+    results = collection.query(
+        # query_texts=[query],  # TODO: look into what multiple queries will do here.
+        n_results=n_results,
+        # where={embed_feature: {"$ne": query}},  # exact match is train/val contamination
+        # TODO: test this:
+        # where_document={"$ne": query},  # exact match is train/val contamination
+    )
+    return results
